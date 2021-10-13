5b3ad043114b2ee6a84f7f3c885aed2979930710
Serega71RUS <serega71rus@gmail.com>
2021-10-13 16:56:25 +0300

    Added POST request to all forms(JSON)

--- js/script.js
+++ js/script.js
@@ -205,6 +205,53 @@ window.addEventListener('DOMContentLoaded', () => {
         "menu__item"
     );
 
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+    const messsage = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = messsage.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+            const object = {};
+            formData.forEach((value, key) => {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+            request.send(json);
+            request.addEventListener('load', () => {
+                if(request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = messsage.success;
+                    form.reset();
+                    setTimeout(() => {statusMessage.remove();}, 2000);
+                }
+                else{
+                    statusMessage.textContent = messsage.failure;
+                }
+            });
+        });
+    }
+
 
     hideTabContent();
     showTabContent();
